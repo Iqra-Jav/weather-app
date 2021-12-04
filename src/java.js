de0279b4d38e9d3f0e8abef9b3314c0e9293dd2d
Iqra-Jav <90656540+Iqra-Jav@users.noreply.github.com>
2021-12-04 20:20:11 +0000

    created geo location function

--- src/java.js
+++ src/java.js
@@ -26,28 +26,44 @@ if (minutes < 10) {
 let dateTime = document.querySelector(".currentDay");
 dateTime.innerHTML = `${day}, ${hours}:${minutes}`;
 
-//Display search-input city in header
-function updateCity(event) {
-  event.preventDefault();
-
-  let city = document.querySelector(".form-control").value;
-  let h2 = document.querySelector("h2");
-  h2.innerHTML = `${city}`;
+function searchCity(city) {
   let apiKey = `9802d88f6dd188634b8362bf8e2349da`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayWeatherCondition);
 }
-let element = document.querySelector("#search-form");
-element.addEventListener("submit", updateCity);
 
-//Display live temperature of the search input city
-
-function displayTemperature(response) {
+function displayWeatherCondition(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
   let temperature = Math.round(response.data.main.temp);
   let temperatureText = document.querySelector(`#temperature`);
   temperatureText.innerHTML = `${temperature}°`;
 
-  let description = response.data.weather[0].description;
+  let description = response.data.weather[0].main;
   let dayForcast = document.querySelector(".dayForcast");
   dayForcast.innerHTML = `${description}`;
 }
+
+//Display search-input city in header
+function updateCity(event) {
+  event.preventDefault();
+  let city = document.querySelector(".form-control").value;
+  searchCity(city);
+}
+let element = document.querySelector("#search-form");
+element.addEventListener("submit", updateCity);
+
+function searchLocation(position) {
+  let apiKey = `9802d88f6dd188634b8362bf8e2349da`;
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector(".location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("New York");
